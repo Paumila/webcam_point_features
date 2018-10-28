4eaa6ec225b082bfeb3f754047016a7ce3a2272a
Paumila <pau.milaguasch@gmail.com>
2018-10-28 16:47:22 +0100

    Fitxer modificat, el programa només visualitza els Keypoints d'una zona de la imatge

--- src/point_features.cpp
+++ src/point_features.cpp
@@ -9,18 +9,40 @@
 #include <cstdlib>
 #include <vector>
 
+using namespace cv;
+using namespace std;
+
 //consts
-const unsigned int MIN_NUM_FEATURES = 300; //minimum number of point fetaures
+const unsigned int MIN_NUM_FEATURES = 1000; //minimum number of point fetaures
 
 int main(int argc, char *argv[])
 {
     cv::VideoCapture camera; //OpenCV video capture object
     cv::Mat image; //OpenCV image object
-	int cam_id; //camera id . Associated to device number in /dev/videoX
+	  int cam_id; //camera id . Associated to device number in /dev/videoX
     cv::Ptr<cv::ORB> orb_detector = cv::ORB::create(); //ORB point feature detector
     orb_detector->setMaxFeatures(MIN_NUM_FEATURES);
     std::vector<cv::KeyPoint> point_set; //set of point features
     cv::Mat descriptor_set; //set of descriptors, for each feature there is an associated descriptor
+    cv::Mat grayimage;
+    cv::Mat Mask;
+    cv::Mat roi;
+
+/*
+    int h_division;
+    int v_division;
+    int n_cols;
+    int n_rows;
+    int i;
+    int j;
+    int k;
+    int l;
+    int Px;
+    int Py;
+
+*/
+
+    Scalar Color = Scalar(0,255,0); //Canvi de color Scalar(0,255,0)-> Green
 
 	//check user args
 	switch(argc)
@@ -59,21 +81,64 @@ int main(int argc, char *argv[])
 
     //**************** Find ORB point fetaures and descriptors ****************************
 
+        //Crear imatge gris
+
+        cv::cvtColor( image, grayimage, CV_BGR2GRAY );
+
         //clear previous points
         point_set.clear();
 
+        Mat Mask = Mat::zeros(image.size(), CV_8U); // Possem 0 a tota la imatge
+
+        // Comanda per poder moure una Mascara dins la imatge
+
+        Mat roi(Mask, cv::Rect(250,150,220,300)); // Només visualitzem KeyPoints en aquest Rectangle, Rect(X_position, Y_position, Mask_width, Mask_Height)
+
+        // Hem col·locat el rectangle perque nomes detecti la zona central de la webcam (Cara)
+        
+        roi = Scalar(255);
+
+        //El codi per fer un detectAndCompute de tota la imatge amb una Mascara
+
+/*
+        n_cols = image.cols;
+        n_rows = image.rows;
+        h_division = 2;
+        v_division = 2;
+
+        Mask = Mat::zeros((n_rows/h_division),(n_cols/v_division),CV_32F);
+
+        for (i=0; i<= v_division; i++)
+        {
+          for (j=0; j<= h_division; j++)
+          {
+            for (k=0; k<= (n_cols/v_division); k++)
+            {
+              for (l=0; l<= (n_rows/h_division); l++)
+              {
+                Px = i*(n_cols/v_division)+ k;
+                Py = j*(n_rows/h_division)+ l;
+                Mask = Mat(Px,Py,CV_32F);
+                Mask = 255;
+              }
+            }
+          }
+        }
+
+*/
+
         //detect and compute(extract) features
-        orb_detector->detectAndCompute(image, cv::noArray(), point_set, descriptor_set);
+        orb_detector->detectAndCompute(grayimage, Mask, point_set, descriptor_set);
 
         //draw points on the image
-        cv::drawKeypoints( image, point_set, image, 255, cv::DrawMatchesFlags::DEFAULT );
+        cv::drawKeypoints(grayimage, point_set, grayimage, Color, cv::DrawMatchesFlags::DEFAULT );
 
     //********************************************************************
 
         //show image
-        cv::imshow("Output Window", image);
+        cv::imshow("Output Window", grayimage);
 
 		//Waits 30 millisecond to check if 'q' key has been pressed. If so, breaks the loop. Otherwise continues.
     	if( (unsigned char)(cv::waitKey(30) & 0xff) == 'q' ) break;
-    }   
+    }
 }
